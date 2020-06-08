5df2975551a6001ca57965453d713cedd6ba9682
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-08 13:20:58 -0700

    created a test for / path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,15 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  
+  it('handles the / route with hi', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('<h1>hi<h1>');
+      });
+    
+  }); 
+
 
 });
