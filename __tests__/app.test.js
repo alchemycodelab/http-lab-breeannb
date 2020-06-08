87d508b2a17369d43b992455f0c0f2367ff9b9f7
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-08 14:32:29 -0700

    fixing the test while working with Lucia about our parse function issue

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,10 +7,29 @@ describe('createResponse', () => {
     return request(app)
       .get('/')
       .then(res => {
-        expect(res.text).toEqual('<h1>hi<h1>');
+        expect(res.text).toEqual('hi');
       });
-    
   }); 
 
+  it('handles the /echo route with status code 200 and plain text with the request body', () => {
+
+    return request(app)
+      .post('/echo')
+      .then(res => {
+        expect(res.body).toEqual({
+          body: , 
+          status: '200 OK', 
+          contentType: 'text/html'
+        });
+      });
+  }); 
+
+//   it('returns the /red route with red as header', () => {
+//     return request(app)
+//       .get('/')
+//       .then(res => {
+//         expect(res.text).toEqual('<h1>red</h1>');
+//       });
+//   }); 
 
 });
